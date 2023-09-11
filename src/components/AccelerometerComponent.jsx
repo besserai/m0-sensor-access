06f4ab900eaf236a3ac542e46afbae3d95482249
moritz <moritz@besser.ai>
2023-09-11 12:04:54 +0200

    added angle

--- src/components/AccelerometerComponent.jsx
+++ src/components/AccelerometerComponent.jsx
@@ -3,11 +3,17 @@ import React, { useState, useEffect } from 'react';
 
 const AccelerometerComponent = () => {
     const [acceleration, setAcceleration] = useState(null);
+    const [altitude, setAltitude] = useState(null);
 
     useEffect(() => {
         const handleDeviceMotion = (event) => {
             const { accelerationIncludingGravity } = event;
             setAcceleration(accelerationIncludingGravity);
+            // calc altitude angle of phone
+            let alpha = - Math.atan(accelerationIncludingGravity.y / accelerationIncludingGravity.z) + 90;
+            setAltitude(alpha);
+
+
         };
 
         if ('DeviceMotionEvent' in window) {
@@ -30,6 +36,8 @@ const AccelerometerComponent = () => {
                     <p>X: {acceleration.x}</p>
                     <p>Y: {acceleration.y}</p>
                     <p>Z: {acceleration.z}</p>
+                    <h2>Angle:</h2>
+                    <p>Altitude: {altitude}</p>
                 </div>
             ) : (
                 <p>Unable to access accelerometer data.</p>
