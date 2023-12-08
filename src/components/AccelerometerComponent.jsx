199bf50a3123b0a7aaec6fdfb2bb44dc97d4b99a
moritz <moritz@besser.ai>
2023-12-08 17:33:40 +0100

    iOS support

--- src/components/AccelerometerComponent.jsx
+++ src/components/AccelerometerComponent.jsx
@@ -20,6 +20,7 @@ const calcTilt = (x = 0, y = 0, z = 0) => {
 }
 
 const AccelerometerComponent = () => {
+    const [deviceOrientationAccessGranted, setDeviceOrientationAccessGranted] = useState(false);
     const [acceleration, setAcceleration] = useState(null);
     const [manualGs, setManualGs] = useState({ "x": 0, "y": 0, "z": 0 });
     const [altitude, setAltitude] = useState(null);
@@ -30,7 +31,7 @@ const AccelerometerComponent = () => {
 
     useEffect(() => {
         const handleDeviceMotion = (event) => {
-            setLogmsg("handleDeviceMotion");
+            // setLogmsg("handleDeviceMotion");
             const { accelerationIncludingGravity } = event;
             setAcceleration(accelerationIncludingGravity);
             // calc altitude angle of phone
@@ -53,14 +54,100 @@ const AccelerometerComponent = () => {
     }, []);
 
 
+    const requestOrientationAccess = () => {
+
+        return new Promise((resolve, reject) => {
+            const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+            console.log("isSafari:", isSafari)
+            setLogmsg(`isSafari: ${isSafari}`);
+
+            if (isSafari) {
+                DeviceOrientationEvent.requestPermission()
+                    .then(permissionState => {
+                        if (permissionState === 'granted') {
+                            setDeviceOrientationAccessGranted(true);
+                            console.log("Device orientation access has already been granted");
+                        } else if (permissionState === 'prompt') {
+                            console.log("User will be prompted for device orientation access");
+                        } else if (permissionState === 'denied') {
+                            console.log("Device orientation access has been denied");
+                        }
+                        setLogmsg(`state: ${permissionState}`);
+                    })
+                    .catch(console.error);
+            } else if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        // Handle successful geolocation access
+                        const { latitude, longitude } = position.coords;
+                        console.log("Latitude:", latitude);
+                        console.log("Longitude:", longitude);
+                        resolve();
+                        setLogmsg(`access granted on android`);
+                    },
+                    (error) => {
+                        // Handle geolocation access error
+                        console.error("Error requesting geolocation access:", error);
+                        if (error.code === error.PERMISSION_DENIED) {
+                            console.error("User denied the request for Geolocation.");
+                        }
+                        reject(error);
+                    }
+                );
+            } else {
+                // Geolocation is not supported by the browser
+                console.error("Geolocation is not supported");
+                reject(new Error("Geolocation is not supported"));
+            }
+        });
+    };
+
+
+    // const checkPermission = () => {
+    //     if (window.DeviceOrientationEvent) {
+
+    //         console.log("DeviceOrientationEvent is available");
+
+    //         } else {
+    //             navigator.geolocation.getCurrentPosition(
+    //                 (position) => {
+    //                     // Handle successful geolocation access
+    //                     const { latitude, longitude } = position.coords;
+    //                     console.log("Latitude:", latitude);
+    //                     console.log("Longitude:", longitude);
+    //                     resolve();
+    //                 },
+    //                 (error) => {
+    //                     // Handle geolocation access error
+    //                     console.error("Error requesting geolocation access:", error);
+    //                     reject(error);
+    //                 }
+    //             );       }             
+    //     } else {
+    //         console.error('DeviceOrientationEvent not available');
+    //         setLogmsg("DeviceOrientationEvent not available");
+    //     }}
+
     return (
         <div>
-            {acceleration ? (
+            {/* {acceleration ? ( */}
+            {true ? (
                 <div>
                     <h2>Accelerometer Data</h2>
+
+                    {/* <button onClick={() => { DeviceOrientationEvent.requestPermission() }}>Request permission</button> */}
+                    <button onClick={requestOrientationAccess}>Request permission</button>
+                    {/* <button onClick={() => { 
+                        DeviceOrientationEvent.requestPermission().then(response => {
+                            if (response === 'granted') {
+                                setDeviceOrientationAccessGranted(true);
+                            } else {
+                                console.alert("Cannot continue without device orientation access");
+                        }} )}}>Request Device Orientation Access</button> */}
+
                     <p id="accel-info" style={{ "color": "yellow" }}></p>
 
-                    {acceleration.x ? (
+                    {/* {acceleration.x ? (
                         <p>X: {acceleration.x.toFixed(1)}</p>
                     ) : (<></>)}
                     {acceleration.y ? (
@@ -68,13 +155,13 @@ const AccelerometerComponent = () => {
                     ) : (<></>)}
                     {acceleration.z ? (
                         <p>Z: {acceleration.z.toFixed(1)}</p>
-                    ) : (<></>)}
+                    ) : (<></>)} */}
                     {/* <p>Y: {acceleration.y.toFixed(1)}</p>
                     <p>Z: {acceleration.z.toFixed(1)}</p> */}
                     <h2>Angles:</h2>
-                    {altitude ? (
+                    {/* {altitude ? (
                         <p>View Altitude: {altitude.toFixed(0)}° </p>
-                    ) : (<></>)}
+                    ) : (<></>)} */}
 
 
                 </div>
@@ -86,6 +173,7 @@ const AccelerometerComponent = () => {
 
             log:
             <p>{logmsg}</p>
+
         </div>
 
 
